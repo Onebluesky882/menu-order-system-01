464107f2457fe1f7177eef277c09f09b799067af
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-11-08 17:17:17 +0700

    08112024

--- src/Page/Tables/index.tsx
+++ src/Page/Tables/index.tsx
@@ -1,6 +1,3 @@
-import { GlobalContext } from "@/Hooks/GlobalContext";
-import { useContext } from "react";
-
 // const { cart } = useContext(GlobalContext).cartProvider;
 
 export const TableNumber = ({ no }: { no: number }) => {
